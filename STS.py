10cf7abe0025e534a490d08de2915a05d7816b2f
justishawkins <32522747+justishawkins@users.noreply.github.com>
2017-11-29 12:38:55 -0500

    blahblah

---
+++ STS.py
@@ -0,0 +1,22 @@
+input("From a scale of 1 to 10, how would you want the tightness of your shoes to be?")
+def ("shoelace"):
+if tightness==1
+    print("The tightness of your shoelaces is one")
+if tightness==2
+    print("The tightness of your shoelaces is two")
+if tightness==3
+    print("The tightness of your shoelaces is three")
+if tightness==4
+    print("The tightness of your shoelaces is four")
+if tightness==5
+    print("The tightness of your shoelaces is five")
+if tightness==6
+    print("The tightness of your shoelaces is six")
+if tightness==7
+    print("The tightness of your shoelaces is seven")
+if tightness==8
+    print("The tightness of your shoeslaces is eight")
+if tightness==9
+    print("The tightness of your shoelaces is nine")
+if tightness==10
+    print("The tightness of your shoelaces is ten")
